0d8ee68ee89c8a5dbab1328838b7b218148ba47c
Fidel Vera <fevc1988@gmail.com>
2023-12-05 12:41:10 -0300

    version 3

--- src_fidel/app.py
+++ src_fidel/app.py
@@ -1,76 +1,55 @@
 import streamlit as st
+from pickle import load
+import numpy as np
 import pandas as pd
-import sqlite3
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.neighbors import KNeighborsClassifier
-import joblib
+import os
 
-# Load the trained model
-model_path = "../models_fidel/best_knn.plk"
-knn_model = joblib.load(model_path)
+# Load the model
+dt_wine_best = load(open("../models_fidel/dt_wine_best.pkl", "rb"))
 
-# Function to preprocess input data
-def preprocess_input(data):
-    # Your preprocessing steps here
-    one_hot_encoder = OneHotEncoder(sparse=False)
-    categorical_columns = ['product_category', 'gender']
-    pairing_encoded_df = pd.DataFrame(one_hot_encoder.fit_transform(data[categorical_columns]))
-    pairing_encoded_df.columns = one_hot_encoder.get_feature_names_out(categorical_columns)
+# Load label encoders
+with open('../models_fidel/label_encoders.pkl', 'rb') as f:
+    label_encoders = load(f)
+    
+ordinal_mapping = {'18-24': 1, '25-34': 2, '35-44': 3, '45-54': 4, '55+': 5}
 
-    scaler = MinMaxScaler()
-    numeric_columns = ['price', 'age']
-    data[numeric_columns] = scaler.fit_transform(data[numeric_columns])
+wine_data = pd.read_csv("wine_data.csv")
 
-    data = pd.concat([data, pairing_encoded_df], axis=1).drop(categorical_columns, axis=1)
-    data = data.dropna()
+st.title('Wine Recommendation App')
 
-    return data
+# Create input widgets for user input
+edad = st.selectbox('¿En cuál rango de edad te encuentras?', ["18-24", "25-34", "35-44", "45-54", "55+"])
+genero = st.selectbox('Seleciona tu género:', label_encoders['genero'])
+precio = st.slider('¿Cuanto tienes presupuestado para esta compra de vinos?', 4000, 25000)
+maridajes = st.multiselect('¿Con qué deseas maridar este vino?', label_encoders['maridaje'])
+categorias = st.multiselect('Selecciona el tipo de vino que más te gusta:', label_encoders['categorias'])
 
-# Streamlit app
-# Dataframe pairing
-con = sqlite3.connect("../data_fidel/interim/wine_products.db")
+# Encode user inputs using label encoders
+edad_n = ordinal_mapping[edad]
+genero_n = np.where(label_encoders['genero'] == genero)[0][0]
 
-query = "SELECT * FROM pairing;"
+# Initialize a set to store unique predictions
+unique_predictions = set()
 
-pairing_df = pd.read_sql_query(query, con=con)
-con.close()
+# Make predictions for each selected combination of 'maridaje' and 'categorias'
+if st.button('Recomendar'):
+    for categoria in categorias:
+        categoria_n = np.where(label_encoders['categorias'] == categoria)[0][0]
+            
+        for maridaje in maridajes:
+            maridaje_n = np.where(label_encoders['maridaje'] == maridaje)[0][0]
+        
+            # Make a prediction
+            prediction = dt_wine_best.predict([[categoria_n, precio, maridaje_n, genero_n, edad_n]])
+            
+            # Add the prediction to the set
+            unique_predictions.add(label_encoders["productos"][prediction[0]])
 
-# Dataframe paring_agrupado
-pairing_df_agrupado = pd.read_csv("../data_fidel/processed/paring_agrupado.csv")
-
-def main():
-    st.title("Wine Recommender App")
-
-    # Get user input
-    product_category = st.selectbox("Select product category:", ["Category_A", "Category_B", "Category_C"])
-    price = st.slider("Select price:", min_value=0.0, max_value=1.0, step=0.01, value=0.5)
-    age = st.slider("Select age:", min_value=0.0, max_value=1.0, step=0.01, value=0.5)
-    gender = st.selectbox("Select gender:", ["Male", "Female"])
-
-    # Create a DataFrame with user input
-    user_data = pd.DataFrame({
-        'product_category': [product_category],
-        'price': [price],
-        'age': [age],
-        'gender': [gender],
-    })
-
-    # Preprocess user input
-    user_data_processed = preprocess_input(user_data)
-
-    # Make predictions
-    prediction = knn_model.predict(user_data_processed)
-
-    # Display recommended product_name and corresponding image_url
-    recommended_product_name = prediction[0]
-    recommended_product_row = pairing_df_agrupado[pairing_df_agrupado['product_name'] == recommended_product_name].iloc[0]
-    recommended_image_url = pairing_df[pairing_df['product_name'] == recommended_product_name]['image_url'].iloc[0]
-
-    st.subheader("Recommended Wine:")
-    st.write(recommended_product_name)
-
-    # Display the image
-    st.image(recommended_image_url, caption='Recommended Wine Image', use_column_width=True)
-
-if __name__ == "__main__":
-    main()
+st.subheader('Las mejores opciones para tu maridaje son:')
+for i, prediction in enumerate(unique_predictions):
+    # Get the image URL corresponding to the product
+    image_url = wine_data.loc[wine_data['productos'] == prediction, 'url_imagen'].values[0]
+    
+    # Display the product name and image with a specific width
+    st.write(f'Opción {i + 1}: {prediction}')
+    st.image(image_url, width=100)
